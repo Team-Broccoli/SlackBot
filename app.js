d7117cf5ab4a3dc88ed1fcd4fb23bf502ea1c81f
racemaus <64076269+racemaus@users.noreply.github.com>
2021-10-19 10:33:41 +0300

    app running in socket mode

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const { App } = require("@slack/bolt");
 require("dotenv").config();
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.APP_TOKEN
 });
 
 (async () => {
-  const port = 3000
+    const port = 3000
 
-  await app.start(process.env.PORT || port);
-  console.log(`SaleBot is running on port ${port}!`);
+    await app.start(process.env.PORT || port);
+    console.log(`SaleBot is running on port ${port}!`);
 })();
